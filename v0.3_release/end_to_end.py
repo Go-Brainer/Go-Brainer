5feb0f258b0e663fbd6281897b5c01d94d007b4c
Nick Bernstein <skolyr@csu.fullerton.edu>
2019-11-20 03:23:10 -0800

    end_to_end with predict bot collected

---
+++ v0.3_release/end_to_end.py
@@ -0,0 +1,80 @@
+# tag::e2e_imports[]
+import h5py
+import time
+
+import tensorflow as tf
+from keras.models import Sequential
+from keras.layers import Dense
+import os
+
+from dlgo.agent.predict import DeepLearningAgent, load_prediction_agent
+from dlgo.data.parallel_processor import GoDataProcessor
+from dlgo.encoders.sevenplane import SevenPlaneEncoder as XPlaneEncoder
+from dlgo.httpfrontend import get_web_app
+from dlgo.networks import large
+from multiprocessing import freeze_support
+# end::e2e_imports[]
+
+def main():
+    samp = 50
+    epo = 1
+
+    skip_to_load = True
+
+    if not skip_to_load:
+        # tag::e2e_processor[]
+        timestr = time.strftime("%Y%m%d-%H%M%S")
+        if not os.path.isdir("./agents"):
+            os.makedirs("./agents")
+        model_h5filename = "./agents/deep_bot_" + "_s" + str(samp) + "e" + str(epo) + ".h5"
+
+        go_board_rows, go_board_cols = 19, 19
+        nb_classes = go_board_rows * go_board_cols
+        encoder = XPlaneEncoder((go_board_rows, go_board_cols))
+        data_dir = "data/" + encoder.name()
+        if not os.path.isdir(data_dir):
+            os.makedirs(data_dir)
+        processor = GoDataProcessor(encoder=encoder.name(), data_directory=data_dir)
+
+        X, y = processor.load_go_data(num_samples=samp)
+        # end::e2e_processor[]
+
+        # tag::e2e_model[]
+        input_shape = (encoder.num_planes, go_board_rows, go_board_cols)
+        model = Sequential()
+        network_layers = large.layers(input_shape)
+        for layer in network_layers:
+            model.add(layer)
+        try:
+            with tf.device('/device:GPU:0'):
+                model.add(Dense(nb_classes, activation='softmax'))
+                model.compile(loss='categorical_crossentropy', optimizer='adadelta', metrics=['accuracy'])
+
+                model.fit(X, y, batch_size=128, epochs=epo, verbose=1)
+                # end::e2e_model[]
+
+                # tag::e2e_agent[]
+                deep_learning_bot = DeepLearningAgent(model, encoder)
+                deep_learning_bot.serialize(h5py.File(model_h5filename, "w"))
+                # end::e2e_agent[]
+
+                # tag::e2e_load_agent[]
+                model_file = h5py.File(model_h5filename, "r")
+                bot_from_file = load_prediction_agent(model_file)
+
+                web_app = get_web_app({'predict': bot_from_file})
+                web_app.run()
+                # end::e2e_load_agent[]
+        except RuntimeError as e:
+            print(e)
+
+    model_file = h5py.File("./agents/deep_bot_A0.h5", "r")
+    bot_from_file = load_prediction_agent(model_file)
+
+    web_app = get_web_app({'predict': bot_from_file})
+    web_app.run()
+
+if __name__ == '__main__':
+    freeze_support()
+    main()
+
